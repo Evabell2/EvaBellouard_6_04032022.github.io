eeba572b9d6d20eb2b0917b175aca1d247a9e3f6
Bellouard <lyn-n@live.fr>
2022-03-18 13:24:16 +0100

    Commit1 code index

---
+++ JavaScript/utils/contactForm.js
@@ -0,0 +1,8 @@
+const modal = document.getElementById("contact_modal");
+
+function displayModal() {
+    modal.style.display = "block";  
+}
+function closeModal() {
+    modal.style.display = "none";
+}
