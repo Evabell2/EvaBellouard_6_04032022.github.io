2fd0f29afddf4bdc43a97e94ada4df11b40898de
Bellouard <lyn-n@live.fr>
2022-04-01 17:41:00 +0200

    Commit 3 + afficher les données

--- JavaScript/factories/photographer.js
+++ JavaScript/factories/photographer.js
@@ -31,49 +31,38 @@ function photographerFactory(data) {
     return { name, picture, location, tagline, prices, link, getUserCardDOM }
 
 }
-function pagePhotographerFactory(data) {
-    const { name, portrait, city, country, tagline } = data;
+function MediaFactory(data, photographer) {
+    const { title, image, video, likes } = data;
 
-    const picture = `/images/photographers/Profils/${portrait}`;
-    const location = `${city}, ${country}`;
+    const pictureMedia = `/images/photographers/${photographer.name}/${image}`;
+    const videoMedia = `/images/photographers/${photographer.name}/${video}`;
 
-    function getUserCardDOM() {
-        const section = document.createElement( 'section' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h1 = document.createElement( 'h1' );
-        h1.textContent = name;
-        const h2 = document.createElement('h2');
-        h2.textContent = location;
-        const p = document.createElement('p');
-        p.textContent = tagline;
-        section.appendChild(img);
-        section.appendChild(h1);
-        section.appendChild(h2);
-        section.appendChild(p);
-        return (section);
-    }
-    return { name, picture, location, tagline, getUserCardDOM }
-
-}
-function mediaFactory(data) {
-    const { title, image, likes, photographerId } = data;
-
-    const picture = `/images/photographers//${image}`;
-
-    function getUserCardDOM() {
+    function getUserMedia() {
         const article = document.createElement( 'article' );
+
+        const a = document.createElement( 'a' );
+        a.setAttribute("href", pictureMedia);
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", pictureMedia); 
+        const video = document.createElement( 'video' );
+        video.setAttribute("src", videoMedia); 
+
+        const div = document.createElement('div');
         const h3 = document.createElement('h3');
         h3.textContent = title;
         const p = document.createElement('p');
         p.textContent = likes;
-        article.appendChild(img);
-        article.appendChild(h3);
-        article.appendChild(p);
+        if (video.src.includes('mp4')) {
+            article.appendChild(video);
+        }
+        else {
+            article.appendChild(a);
+            a.appendChild(img);
+        }
+        article.appendChild(div);
+        div.appendChild(h3);
+        div.appendChild(p);
         return (article);
     }
-    return { title, picture, likes, getUserCardDOM }
-
+    return { title, pictureMedia, likes, videoMedia, getUserMedia }
 }
